d1c3f338612293fb9dbf0f0fc475d3d16ed5c663
SABA Goulwindin Nayim Isaack <sabanayim10@gmail.com>
2026-05-08 19:42:07 +0100

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:5174'],
+    'allowed_origins' => ['http://localhost:5173', 'http://localhost:5174', 'https://mon-projet-frontend-8682.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
